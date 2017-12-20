88fe746e5fa5bc521e7186862e140be8247fc5f0
thiagolucio <thiagolucio@gmail.com>
2017-12-20 21:11:33 -0200

    primeiro commit  para poc gama

---
+++ timeline/js/custom.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+/* off-canvas sidebar toggle */
+$('[data-toggle=offcanvas]').click(function() {
+    $('.row-offcanvas').toggleClass('active');
+    $('span.collapse').toggleClass('in');
+});
+
+$('[data-toggle=offcanvas-in]').click(function() {
+    $('.row-offcanvas').addClass('active');
+    $('span.collapse').addClass('in');
+});
+
+});
